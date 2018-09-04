e3346b6b128c1c8c745a894c9eafbae666a48cb2
zes <cyril.gerla@gmail.com>
2018-09-04 19:41:31 +0200

    [chore] add redux store
    redux

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  const props = {
+    store: {
+      dispatch: jest.fn(),
+      getState: jest.fn(),
+      subscribe: jest.fn(),
+    },
+  };
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App {...props} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
